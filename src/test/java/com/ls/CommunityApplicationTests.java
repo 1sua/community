0d6d54e75ed5fb1dd294693d25102ebdbedc119d
lsua <1725248162@qq.com>
2020-08-31 15:51:29 +0800

    second commit

--- src/test/java/com/ls/CommunityApplicationTests.java
+++ src/test/java/com/ls/CommunityApplicationTests.java
@@ -1,13 +1,34 @@
 package com.ls;
 
+import com.ls.util.MailClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @SpringBootTest
 class CommunityApplicationTests {
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     void contextLoads() {
+        mailClient.sendMail("1725248162@qq.com","text","welcome");
+    }
+
+    @Test
+    void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username","sunday");
+        String content = templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+
+        mailClient.sendMail("lsua@qq.com","HTML",content);
     }
 
 }
